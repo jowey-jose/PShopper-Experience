de5fb4553107e7421767a9b5c983f9ed5ac67800
Jowey-jose <josephmbuku17@gmail.com>
2022-03-12 17:59:02 +0300

    ezra code debug and build

--- app-server.js
+++ app-server.js
@@ -45,7 +45,8 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  // res.json({ message: "Welcome to PShopper Experience application." });
+  res.sendFile(__dirname, "./index.html")
 });
 
 // routes
